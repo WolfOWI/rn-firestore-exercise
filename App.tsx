791133784899e7a4e6d54981680898659eb475ea
Armand Pretorius <80468905+Armand-OW@users.noreply.github.com>
2025-07-22 12:20:25 +0200

    setup of demonstration

--- App.tsx
+++ App.tsx
@@ -1,20 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import ListScreen from './screens/ListScreen';
+import DetailsScreen from './screens/DetailsScreen';
+import CreateScreen from './screens/CreateScreen';
+
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Home'>
+        <Stack.Screen name="Home" component={ListScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+        <Stack.Screen name="Add" component={CreateScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ firebase.tsx
@@ -0,0 +1,3 @@
+//TODO: Initialise Cloud Firestore and get a reference to the service
+//Documentation: https://docs.expo.dev/guides/using-firebase/
+
