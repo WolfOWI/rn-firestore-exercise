4fff0fac925792241701aea29857b32aba6e4fd8
WolfOWI <97434628+WolfOWI@users.noreply.github.com>
2025-07-30 10:26:45 +0200

    Establish Firebase Connection using dotEnv

--- firebase.tsx
+++ firebase.tsx
@@ -1,3 +1,38 @@
-//TODO: Initialise Cloud Firestore and get a reference to the service
-//Documentation: https://docs.expo.dev/guides/using-firebase/
+import { initializeApp } from "firebase/app";
+import { addDoc, collection, getFirestore } from "firebase/firestore";
+import {
+  FIREBASE_API_KEY,
+  FIREBASE_AUTH_DOMAIN,
+  FIREBASE_PROJECT_ID,
+  FIREBASE_STORAGE_BUCKET,
+  FIREBASE_MESSAGING_SENDER_ID,
+  FIREBASE_APP_ID,
+} from "@env";
 
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: FIREBASE_API_KEY,
+  authDomain: FIREBASE_AUTH_DOMAIN,
+  projectId: FIREBASE_PROJECT_ID,
+  storageBucket: FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+  appId: FIREBASE_APP_ID,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialise Cloud Firestore and get a reference to the service
+export const db = getFirestore(app);
+
+export const testAddData = async () => {
+  try {
+    const docRef = await addDoc(collection(db, "test"), {
+      name: "John Doe",
+      age: 30,
+    });
+    console.log("Document written with ID: ", docRef.id);
+  } catch (e) {
+    console.error("Error adding document: ", e);
+  }
+};
--- screens/CreateScreen.tsx
+++ screens/CreateScreen.tsx
@@ -1,89 +1,100 @@
-import { StyleSheet, Text, View, SafeAreaView, TextInput, TouchableOpacity, Switch } from 'react-native'
-import React, { useState } from 'react'
-import { useNavigation } from '@react-navigation/core';
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  TextInput,
+  TouchableOpacity,
+  Switch,
+} from "react-native";
+import React, { useState } from "react";
+import { useNavigation } from "@react-navigation/core";
+import { testAddData } from "../firebase";
 
 const CreateScreen = () => {
-    const navigation:any = useNavigation();
+  const navigation: any = useNavigation();
 
-    const [title, setTitle] = useState('')
-    const [priority, setPriority] = useState(false)
-    const [due, setDue] = useState('')
-    const [description, setDescription] = useState('')
+  const [title, setTitle] = useState("");
+  const [priority, setPriority] = useState(false);
+  const [due, setDue] = useState("");
+  const [description, setDescription] = useState("");
 
   return (
-    <SafeAreaView >
-        <View style={styles.container}>
+    <SafeAreaView>
+      <View style={styles.container}>
+        <TextInput
+          style={styles.inputField}
+          placeholder="Bucket List Title"
+          onChangeText={(newText) => setTitle(newText)}
+          defaultValue={title}
+        />
 
-            <TextInput
-                style={styles.inputField}
-                placeholder="Bucket List Title"
-                onChangeText={newText => setTitle(newText)}
-                defaultValue={title}
-            />
+        <TextInput
+          style={styles.inputField}
+          placeholder="When do you want it done?"
+          onChangeText={(newText) => setDue(newText)}
+          defaultValue={due}
+        />
 
-            <TextInput
-                style={styles.inputField}
-                placeholder="When do you want it done?"
-                onChangeText={newText => setDue(newText)}
-                defaultValue={due}
-            />      
+        <TextInput
+          multiline
+          numberOfLines={4}
+          style={styles.inputField}
+          placeholder="Description of bucket list"
+          onChangeText={(newText) => setDescription(newText)}
+          defaultValue={description}
+        />
 
-            <TextInput
-                multiline
-                numberOfLines={4}
-                style={styles.inputField}
-                placeholder="Description of bucket list"
-                onChangeText={newText => setDescription(newText)}
-                defaultValue={description}  
-            />
+        <View style={styles.switch}>
+          <Switch
+            trackColor={{ false: "black", true: "green" }}
+            thumbColor={priority ? "yellow" : "white"}
+            ios_backgroundColor="#3e3e3e"
+            onValueChange={(toggle) => setPriority(toggle)}
+            value={priority}
+          />
+          <Text>Priority?</Text>
+        </View>
 
-            <View style={styles.switch}>
-                <Switch
-                    trackColor={{false: 'black', true: 'green'}}
-                    thumbColor={priority ? 'yellow' : 'white'}
-                    ios_backgroundColor="#3e3e3e"
-                    onValueChange={(toggle) => setPriority(toggle)}
-                    value={priority}
-                />
-                <Text>Priority?</Text>
-            </View>
+        <TouchableOpacity style={styles.button} onPress={testAddData}>
+          <Text style={styles.buttonText}>Test Firebase Add Data</Text>
+        </TouchableOpacity>
 
-            <TouchableOpacity style={styles.button} >
-                <Text style={styles.buttonText}>Create Bucket List Item</Text>
-            </TouchableOpacity>
-        
-        </View>  
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Create Bucket List Item</Text>
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
-  )
-}
+  );
+};
 
-export default CreateScreen
+export default CreateScreen;
 
 const styles = StyleSheet.create({
-    container: {
-        padding: 20
-    },
-    inputField: {
-        borderWidth: 2,
-        borderColor: 'black',
-        marginTop: 15,
-        padding: 10
-    },
-    button: {
-        backgroundColor: "green",
-        textAlign: 'center',
-        padding: 15,
-        marginTop: 30
-    },
-    buttonText: {
-        textAlign: 'center',
-        color: 'white'
-    },
-    switch: {
-        marginTop: 15,
-        display: 'flex',    
-        flexDirection: 'row-reverse',
-        alignItems: 'center',
-        gap: 10,
-    }
-})
+  container: {
+    padding: 20,
+  },
+  inputField: {
+    borderWidth: 2,
+    borderColor: "black",
+    marginTop: 15,
+    padding: 10,
+  },
+  button: {
+    backgroundColor: "green",
+    textAlign: "center",
+    padding: 15,
+    marginTop: 30,
+  },
+  buttonText: {
+    textAlign: "center",
+    color: "white",
+  },
+  switch: {
+    marginTop: 15,
+    display: "flex",
+    flexDirection: "row-reverse",
+    alignItems: "center",
+    gap: 10,
+  },
+});
